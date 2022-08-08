b2e9f1725e4b2717d44a499a4cba3aa170f3d4b9
PC <PC@DESKTOP-MSN2FHL>
2022-08-09 00:16:03 +0300

    Add project files.

---
+++ Engine.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Company
+{
+    class Engine
+    {
+        public double Power { get; set; }
+        public string Manufacturer { get; set; }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Company
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
